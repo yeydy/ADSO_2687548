2db004589e707fe3a2cfae3b309559e4da85bf05
stiven-rincon <137924290+stiven-rincon@users.noreply.github.com>
2023-09-05 14:14:19 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int C = 4 ;
+        float Num=C%2 ;
+        if (Num==0){
+            System.out.println("el numero es par");
+        } else {
+            System.out.println("el numero no es par");
+        }
+    }
+
+}
