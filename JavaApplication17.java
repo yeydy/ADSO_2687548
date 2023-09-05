005b8dcd38e96b49489c1a29348d95586b312737
Gomez420JJ <139909095+Gomez420JJ@users.noreply.github.com>
2023-09-05 14:02:57 -0500

    Add files via upload

---
+++ JavaApplication17.java
@@ -0,0 +1,19 @@
+
+package javaapplication17;
+
+
+public class JavaApplication17 {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int c=6 ;float num=c%2;
+        if(num==0){
+            System.out.println("el numero es par");
+        }else{
+            System.out.println("el numero no es par");
+        }
+    
+    }
+    
+}
