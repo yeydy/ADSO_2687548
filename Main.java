3594d4d0c120091e394acc0a5d44f30fcec4cca7
stiven-rincon <137924290+stiven-rincon@users.noreply.github.com>
2023-09-05 14:03:00 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("vamos a saber que sueldo es mayor");
+        int sueldoactual=500000;
+        int sueldominimo=200000;
+
+        if(sueldoactual>sueldominimo) {
+            System.out.println("el sueldo actual es mayor al sueldo minimo");
+        } else {
+            System.out.println("el sueldo minimo es mayor al actual");
+        }
+    }
+    }
