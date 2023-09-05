4193b46d41a2d612c6b6756409375a90ce11bcf2
xXjunior02 <105760585+xXjunior02@users.noreply.github.com>
2023-09-05 13:46:50 -0500

    Add files via upload

---
+++ actividad1/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+     int Sueldo_minimo=1500000;
+     int Sueldo_maximo=1800000;
+     if(Sueldo_maximo>Sueldo_minimo){
+         System.out.println("el sueldo maximo es mayor");
+     }else{
+         System.out.println("el sueldo minimo es mayor");
+     }
+    }
+
+}
