6f0130367a7936b067238e2ef33be2ea6bbf2520
prado <127165122+prado0909@users.noreply.github.com>
2023-09-05 13:54:40 -0500

    Add files via upload

---
+++ JAVA/Ejercicio calificado/src/main/java/com/mycompany/correccion/Correccion.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.correccion;
+
+public class Correccion {
+
+    public static void main(String[] args) {
+        int c = 4;
+        float num = c%2;
+        if(num == 0){
+            System.out.println("El numero es par");
+        }else{
+            System.out.println("El numero no es par");
+        }
+    }
+}
