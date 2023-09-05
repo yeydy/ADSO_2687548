54473670fc8fc7fa1092a4bbcd9e6d3d3d0f89f5
anthonymmp <139909019+anthonymmp@users.noreply.github.com>
2023-09-05 14:02:12 -0500

    Add files via upload
    evaluacion

---
+++ ejercicio1.java
@@ -0,0 +1,13 @@
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int c = 3;
+        if (c % 2 == 0) {
+            System.out.println("el numero es par");
+
+        }
+    else{
+            System.out.println("el numero es impar");
+
+        }
+    }
+}
---
+++ ejercicio2.java
@@ -0,0 +1,15 @@
+
+
+public class ejercicio2 {
+    public static void main (String[]args) {
+        int SueldoActual = 1300;
+        int SueldoMinimo = 1200;
+        if (SueldoMinimo > SueldoActual) {
+            System.out.println("el sueldo actual es mayor al sueldo minimo");
+
+        }
+        else{
+            System.out.println("el sueldo actual es menor al sueldo minimo");
+        }
+    }
+}
