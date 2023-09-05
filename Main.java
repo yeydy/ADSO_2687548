a7422143afff948af4ce4ace3fa98fc371a93c2b
stiven-rincon <137924290+stiven-rincon@users.noreply.github.com>
2023-09-05 14:18:00 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
-    public static void main(String[] args){
-        int C = 4 ;
-        float Num=C%2 ;
-        if (Num==0){
-            System.out.println("el numero es par");
+    public static void main(String[] args) {
+        System.out.println("vamos a saber que sueldo es mayor");
+        int sueldoactual=500000;
+        int sueldominimo=200000;
+
+        if(sueldoactual>sueldominimo) {
+            System.out.println("el sueldo actual es mayor al sueldo minimo");
         } else {
-            System.out.println("el numero no es par");
+            System.out.println("el sueldo minimo es mayor al actual");
         }
     }
-
-}
+    }
