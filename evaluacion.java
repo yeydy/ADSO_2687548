3e39161905c4a93b93f3bba61c443326c34e86ba
Keiner2031 <137812906+Keiner2031@users.noreply.github.com>
2023-09-05 19:18:30 -0500

    Add files via upload

---
+++ evaluacion.java
@@ -0,0 +1,21 @@
+package ejercicio1;
+
+public class evaluacion {
+
+/**
+ * @param args
+ */
+public static void main(String[] args) {
+
+
+        float SueldoC = 3250;
+        float SueldoMin = 2220;
+        if (SueldoC > SueldoMin) {
+            System.out.println("el sueldo es mayor al minimo " );
+        } else {
+            System.out.println("el valor es menor al minimo ");
+        }
+
+
+    }
+}
