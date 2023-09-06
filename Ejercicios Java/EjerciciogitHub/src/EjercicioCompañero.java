a4dbd4612a266305cc32c72e4d1eec3d778dfb6d
Ivan-Mejia <ivan10villabona@gmail.com>
2023-09-05 21:46:58 -0500

    Subiendo archivos de Java

---
+++ Ejercicios Java/EjerciciogitHub/src/EjercicioCompañero.java
@@ -0,0 +1,12 @@
+public class EjercicioCompañero {
+        public static void main(String[] args) {
+            int sueldoActual = 1300;
+            int sueldoMinimo = 1200;
+            if (sueldoActual > sueldoMinimo) {
+                System.out.println("El sueldo actual es mayor al sueldo minimo");
+            } else {
+                System.out.println("El sueldo actual es menor al sueldo minimo");
+            }
+
+        }
+}
---
+++ Ejercicios Java/EjerciciogitHub/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int variableC=10;
+        if (variableC%2==0){
+            System.out.println("El numero es par");
+        }
+        else
+        {
+            System.out.println("El numero es impar");
+        }
+    }
+
+}
