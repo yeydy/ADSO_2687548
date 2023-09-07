ffa280692ca9d4699aa62a2439f73b8943b6f21c
2005anndrea <patinocatropaulaandrea@gmail.com>
2023-09-06 19:04:56 -0500

    ejercicio de java #1

---
+++ ejercicio#01java/Ejercicios.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+
+/**
+ *
+ * @author SENA
+ */
+public class Ejercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int sueldo=100;
+       int minimo=300;
+       if ( sueldo>minimo){
+           System.out.println(" sueldo es mayor a minimo ");
+       } 
+       else {
+           System.out.println(" minimo es mayor a sueldo ");
+       }
+    }
+    
+}
---
+++ ejercicio#01java/ejercicio_compañero.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+
+/**
+ *
+ * @author SENA
+ */ 
+public class ejercicio_compañero {
+ public static void main(String[] args) {
+    int numero=9;
+    float residuo=2;
+    if ( numero%residuo==0){
+     System.out.println(" el numero es par ");
+}
+    else {
+            System.out.println(" no es par ");
+
+}
+}
+}
+
